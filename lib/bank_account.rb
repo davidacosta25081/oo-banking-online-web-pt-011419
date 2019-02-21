2b88a1f073f3dc65719d20284b22ba6adb6d124d
David Acosta <davidacosta25081@yahoo.com>
2019-02-21 07:44:47 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,27 +1,28 @@
 class BankAccount
 
-attr_reader :name, :balance, :status
+  attr_reader :name, :balance, :status
 
-def initialize (name,balance=1000)
-  @name = name
-  @balance = balance
-  @status = "open"
-end
+  def initialize (name,balance=1000)
+    @name = name
+    @balance = balance
+    @status = "open"
+  end
 
-def deposit(amount)
-  @balance +=amount
-end
+  def deposit(amount)
+    @balance +=amount
+  end
 
-def display_balance
-   "Your balance is $#{@balance}."
-end
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
 
-def valid?
-  @status == "open"  &&  @balance > 0 ? true : false
-end
-
-
+  def valid?
+    @status == "open"  &&  @balance > 0 ? true : false
+  end
 
 
+  def close_account
+    @status = "closed"
+  end  
 
 end
