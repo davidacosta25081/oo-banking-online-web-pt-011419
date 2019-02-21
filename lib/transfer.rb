f2bcfa8bd6a5bf42a0d92f03433b9b0c38fb53c9
David Acosta <davidacosta25081@yahoo.com>
2019-02-21 08:09:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,4 +14,23 @@ attr_accessor :sender, :receiver, :amount, :status
     @sender.valid? && @receiver.valid? ? true : false
   end
 
-end
+  def execute_transaction
+     if @sender.balance > @amount && @status == "pending"
+       @sender.balance -= @amount
+       @receiver.balance += @amount
+       @status = "complete"
+     else
+       @status = "rejected"
+       return "Transaction rejected. Please check your account balance."
+     end
+   end
+
+
+   def reverse_transfer
+     if @status == "complete"
+       @sender.balance += @amount
+       @receiver.balance -= @amount
+       @status = "reversed"
+     end
+   end
+ end
